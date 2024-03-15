c22ccaca232538db05f85351290c6428deabcb24
Euan <sj19330@bristol.ac.uk>
2024-03-15 23:19:41 +1100

    moved loading to app.js from homepage. Also got rid of the database stuff from homepage, moving to app.js

--- App.js
+++ App.js
@@ -7,19 +7,34 @@ import WeightLog from "./Pages/WeightLog";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import TestPage from "./Pages/Testpage";
+import { useState, useEffect } from "react";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Track Food" component={TrackFood} />
-        <Stack.Screen name="Progress" component={Progress} />
-        <Stack.Screen name="Weight Log" component={WeightLog} />
-        <Stack.Screen name="Test" component={TestPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000);
+  });
+
+  if (isLoading) {
+    return (
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+        <Text>Loading data from database...</Text>
+      </View>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomePage} />
+          <Stack.Screen name="Track Food" component={TrackFood} />
+          <Stack.Screen name="Progress" component={Progress} />
+          <Stack.Screen name="Weight Log" component={WeightLog} />
+          <Stack.Screen name="Test" component={TestPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -8,102 +8,40 @@ export default function HomePage() {
   const developerMode = true;
   const nav = useNavigation();
   const db = SQLite.openDatabase("data");
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => setIsLoading(false), 1000);
-    db.transaction((tx) => {
-      tx.executeSql(
-        "CREATE TABLE IF NOT EXISTS slidersData(date DATE, breakfast INTEGER, lunch INTEGER, dinner INTEGER, snacks INTEGER, time TIME)",
-        [],
-        (sqlTxn, resultSet) => {
-          console.log("table created successfully");
-        },
-        (error) => {
-          console.log("error whilst creating dataset");
-        }
-      );
-    });
-    db.transaction((tx) => {
-      tx.executeSql(
-        "INSERT INTO slidersData(breakfast, lunch, dinner) VALUES (1,4,2)",
-        [],
-        (sqlTxn, resultSet) => {
-          console.log("values added successfully");
-        },
-        (error) => {
-          console.log("error whilst adding values 142");
-        }
-      );
-    });
-    db.transaction((tx) => {
-      tx.executeSql(
-        "CREATE TABLE IF NOT EXISTS weightData(date DATE, weight FLOAT(24), time TIME)",
-        [],
-        (sqlTxn, resultSet) => {
-          console.log("table created successfully");
-        },
-        (error) => {
-          console.log("error whilst creating dataset");
-        }
-      );
-    });
-  }, []);
-
-  const getData = () => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "SELECT * FROM slidersData",
-        [],
-        (txobj, resultSet) => console.log(resultSet),
-        (error) => {
-          console.log("cannot fetch results set");
-        }
-      );
-    });
-  };
-
-  if (isLoading) {
-    return (
-      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
-        <Text>Loading data from database...</Text>
+  return (
+    <View style={styles.page}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Welcome to Counter</Text>
       </View>
-    );
-  } else {
-    return (
-      <View style={styles.page}>
-        <View style={styles.headerContainer}>
-          <Text style={styles.headerText}>Welcome to Counter</Text>
-        </View>
-        <View style={styles.body}>
+      <View style={styles.body}>
+        <CustomButton
+          width="60%"
+          text="Track Food"
+          onPress={() => nav.navigate("Track Food")}
+        />
+        <CustomButton
+          width="60%"
+          text="Log Weight"
+          onPress={() => nav.navigate("Weight Log")}
+        />
+        <CustomButton
+          width="60%"
+          text="View Progress"
+          onPress={() => nav.navigate("Progress")}
+        />
+        {developerMode && (
           <CustomButton
             width="60%"
-            text="Track Food"
-            onPress={() => nav.navigate("Track Food")}
+            text="Test Page"
+            onPress={() => {
+              getData();
+            }}
           />
-          <CustomButton
-            width="60%"
-            text="Log Weight"
-            onPress={() => nav.navigate("Weight Log")}
-          />
-          <CustomButton
-            width="60%"
-            text="View Progress"
-            onPress={() => nav.navigate("Progress")}
-          />
-          {developerMode && (
-            <CustomButton
-              width="60%"
-              text="Test Page"
-              onPress={() => {
-                getData();
-              }}
-            />
-          )}
-        </View>
+        )}
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
