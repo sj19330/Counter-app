a782ef539a08826756634d25c977d0cc86783238
euanbagguley <ebagguley1@gmail.com>
2024-07-18 11:44:55 +0100

    fixed duplicate bugs from rebasing

--- Pages/Testpage.js
+++ Pages/Testpage.js
@@ -1,17 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
-
 import { useNavigation } from "@react-navigation/native";
-import { useContext, useEffect, useState } from "react";
 import { useContext, useEffect, useState } from "react";
 import { DBProvider } from "../DbProvider";
 
 export default function TestPage() {
   const db = useContext(DBProvider);
-  const [content, setContent] = useState(null);
-
-  useEffect(() => {
-    getContent(db).then((content) => setContent(content));
-  }, []);
   const [content, setContent] = useState(null);
 
   useEffect(() => {
@@ -25,18 +18,6 @@ export default function TestPage() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-async function getContent(db) {
-  const content = await db.getAllAsync("SELECT * FROM foodLog");
-  return content[content.length - 1].breakfast;
-}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
